220c757f2e69f7197bc0d393a879c29c5232c4ad
HumonFromWalnart <tlmuunulzii@gmail.com>
2022-10-13 19:50:43 +0800

    just started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+
+import jesus from './jesus-ballin.gif'
+import mettaton from './Mettaton_NEO_battle.gif'
 import './App.css';
+// import { createElement } from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={jesus} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          jesus <code>ballin</code>.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://freerobux.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          100% Proven By Jesus Itself
         </a>
       </header>
+      <div className='bigOne'>
+        <div id='container'>
+          <div id='Head'></div>
+          <div className='inputinTime'>
+            <div className='rowinTime'>
+            <input id='input'></input>
+            <button id="button" onClick={getValue}>Add</button>
+            </div>
+            <div id='new'></div>
+            <img src={mettaton} className="App-logo" alt="logo" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
+
+const getValue = () => {
+  const inputValue2 = document.getElementById("input").value;
+  add(inputValue2);
+}
+  document.addEventListener("keydown", (element) => {
+    if (element.key == "Enter") {
+        const inputValue = document.getElementById("input").value;
+        add(inputValue);
+    }
+});
+
+let counter = 0;
+
+const add = (value) => {
+  counter += 1;
+  const newElement = document.createElement("div");
+  newElement.innerHTML = `${counter}. ${value} `;
+  newElement.style = "margin-top: 10px; height: 30px; font-size: 25px; border: 1px solid rgb(77, 30, 30); border-radius: 15px; display: flex; align-items: center; margin-bottom:30px"
+  console.log(counter)
+  document.getElementById('new').appendChild(newElement);
+  document.getElementById('new').style.display = "block"
+}
 export default App;
